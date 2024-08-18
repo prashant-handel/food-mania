16905ec07c8c88b4d45a2d490e44bb125d7774ba
prashant-handel <prashanthandel2501@gmail.com>
2024-08-18 16:34:08 +0530

    completed upto login and logout

---
+++ frontend/src/app/components/pages/home/home.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { FoodService } from 'src/app/services/food.service';
+import { Food } from 'src/app/shared/models/Food';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+  foods:Food[] = [];
+  
+  constructor(private foodService: FoodService, activatedRoute: ActivatedRoute) {
+    activatedRoute.params.subscribe((params) => {
+      let foodObservable:Observable<Food[]>;
+      if (params.searchTerm)
+        foodObservable = this.foodService.getAllFoodsBySearchTerm(params.searchTerm);
+      else if (params.tag)
+        foodObservable = this.foodService.getAllFoodsByTag(params.tag);
+      else
+      foodObservable = foodService.getAll();
+
+      foodObservable.subscribe((foodFromApi)=> {
+        this.foods = foodFromApi;
+      })
+    })
+  }
+
+  ngOnInit(): void {
+  }
+
+}
